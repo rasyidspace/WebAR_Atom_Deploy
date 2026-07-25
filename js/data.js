9777778989a7d2ab1a3f36beb8288b69d71f4c33
Rasyid <rasyidtarmizi2@gmail.com>
2026-07-25 13:07:03 +0700

    Redesign material cards to use thumbnail backgrounds

--- js/data.js
+++ js/data.js
@@ -22,18 +22,14 @@ function renderMateriCards(atomsData) {
     
     atomsData.forEach(atom => {
         const cardHtml = `
-        <div class="material-card theme-${atom.id}">
-          <div class="mc-header">
-            <div class="mc-icon">
-              ${atom.iconSvg}
-            </div>
+        <div class="material-card theme-${atom.id}" style="background-image: url('${atom.thumbnail}');">
+          <div class="mc-content">
             <h3 class="card-title">${atom.name}</h3>
+            <button class="btn btn-secondary btn-sm w-100" onclick="window.location.href='materi-detail.html?atom=${atom.id}'">
+              <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/></svg>
+              Buka Materi
+            </button>
           </div>
-          <p class="card-desc">${atom.description}</p>
-          <button class="btn btn-outline btn-sm w-100 mt-2" onclick="window.location.href='materi-detail.html?atom=${atom.id}'">
-            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/></svg>
-            Buka Materi
-          </button>
         </div>
         `;
         container.insertAdjacentHTML('beforeend', cardHtml);
